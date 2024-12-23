2a1213838fee78bb4280735b78a7af49680b2092
mjavurek <57668028+mjavurek@users.noreply.github.com>
2024-12-23 16:59:13 +0100

    Update user_serializer.rb: add ordergroup_id (#6)

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :locale
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :locale, :ordergroup_name, :ordergroup_id
+
+  def ordergroup_id
+    object.ordergroup&.id
+  end
+end
 end
